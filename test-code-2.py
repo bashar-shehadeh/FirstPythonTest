ecdb4dffb9fa00bff9c2ec0c0e184b44ae717211
Bashar Shehadeh <bashar.sh@outlook.com>
2020-08-30 16:23:49 -0500

    Adding a second file

---
+++ test-code-2.py
@@ -0,0 +1,6 @@
+print('Give me two numbers') 
+print('Number 1') 
+numOne = input()
+print('Number 2') 
+numTwo = input()
+print('Magic!! I guessed your numbers! They are {} and {}'.format(numOne, numTwo))
